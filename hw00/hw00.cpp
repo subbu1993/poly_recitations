59d3b7fe440380b303593495c16a1f4f0adb69a4
Subramaniam Swamynathan <ramani1993@gmail.com>
2015-09-13 01:38:25 -0400

    everything in main in hw00

---
+++ hw00/hw00.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<string>
+using namespace std;
+
+int main()
+{
+  ifstream ifs("encrypted.txt");
+  if(!ifs)
+  {
+    cerr << "Could not open the file";
+    exit(1);
+  }
+
+  int numberOfRotations;
+  string line;
+  getline(ifs,line);
+  ifs >> numberOfRotations;
+  vector<string> text;
+  while(getline(ifs,line))
+  {
+    text.push_back(line);
+  }
+  ifs.close();
+  for(size_t i=0;i<text.size();i++)
+  {
+    string encrypted_line = text[i];
+    for(int j=0;j<encrypted_line.size();j++)
+    {
+      char encrypted = encrypted_line[j];
+      if (encrypted > 'a' && encrypted != ' ')
+      {
+        if(encrypted - numberOfRotations < 'a')
+        {
+          int diff = encrypted_line[j] - 'a';
+          diff++;
+          encrypted_line[j] = 'z' - (numberOfRotations - diff);
+        }
+        else
+          encrypted_line[j] = encrypted - numberOfRotations;
+      }
+      text[i] = encrypted_line;
+    }
+  }
+
+  for(size_t i = text.size() - 1 ; i>= 0; i-- )
+  {
+    cout << text[i] << endl;
+  }
+}
