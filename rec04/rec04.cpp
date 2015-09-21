0d99e4b816be02335c207864881f917f0ec062a6
Subramaniam Swamynathan <ramani1993@gmail.com>
2015-09-21 15:46:03 -0400

    added type Sandwich

--- rec04/rec04.cpp
+++ rec04/rec04.cpp
@@ -3,3 +3,10 @@
   Subbu
   Modelling a sandwich making and delivering company and ensuring proper return of the delivery trucks
 */
+
+class Sandwich1
+{
+  int numberOfBread,quantityOfCheese,amountOfMayo,numberOfTomatoes;
+  double amountOfMustard;
+
+}
