fc8d88e940034ab434ada66b0cf34f6aa2791ba1
Subramaniam Swamynathan <ramani1993@gmail.com>
2015-09-21 16:15:24 -0400

    took out the int assign

--- rec04/rec04.cpp
+++ rec04/rec04.cpp
@@ -1,29 +1,30 @@
 /*
-  Rec04
-  Subbu
-  Modelling a sandwich making and delivering company and ensuring proper return of the delivery trucks
+Rec04
+Subbu
+Modelling a sandwich making and delivering company and ensuring proper return of the delivery trucks
 */
 #include<iostream>
 using namespace std;
 
 class Sandwich1
 {
-  int numberOfBread,quantityOfCheese,amountOfMayo,numberOfTomatoes;
-  double amountOfMustard;
+	int numberOfBread, quantityOfCheese, numberOfTomatoes;
+	double amountOfMustard;
+	double amountOfMayo = 0.1;
 
-  public:
-    void display()
-    {
-      cout << "Here are the contents of my sandwich" << endl;
-      cout << "Bread: " << numberOfBread << endl;
-      cout << "Cheese: " << quantityOfCheese << endl;
-      cout << "Mayo: " << amountOfMayo << endl;
-      cout << "Tomoatoes: " << numberOfTomatoes << endl;
-    }
+public:
+	void display()
+	{
+		cout << "Here are the contents of my sandwich" << endl;
+		cout << "Bread: " << numberOfBread << endl;
+		cout << "Cheese: " << quantityOfCheese << endl;
+		cout << "Mayo: " << amountOfMayo << endl;
+		cout << "Tomoatoes: " << numberOfTomatoes << endl;
+	}
 };
 
 int main()
 {
-  Sandwich1 s1;
-  s1.display();
+	Sandwich1 s1;
+	s1.display();
 }
