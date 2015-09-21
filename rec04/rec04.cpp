46d8c6a3b7873ee08f51bab436d386ee9978817c
Subramaniam Swamynathan <ramani1993@gmail.com>
2015-09-21 16:05:20 -0400

    added display method and getting the garbage values

--- rec04/rec04.cpp
+++ rec04/rec04.cpp
@@ -3,16 +3,27 @@
   Subbu
   Modelling a sandwich making and delivering company and ensuring proper return of the delivery trucks
 */
+#include<iostream>
+using namespace std;
 
 class Sandwich1
 {
   int numberOfBread,quantityOfCheese,amountOfMayo,numberOfTomatoes;
   double amountOfMustard;
 
+  public:
+    void display()
+    {
+      cout << "Here are the contents of my sandwich" << endl;
+      cout << "Bread: " << numberOfBread << endl;
+      cout << "Cheese: " << quantityOfCheese << endl;
+      cout << "Mayo: " << amountOfMayo << endl;
+      cout << "Tomoatoes: " << numberOfTomatoes << endl;
+    }
 };
 
 int main()
 {
   Sandwich1 s1;
-
+  s1.display();
 }
