4f2521a13199e312c97558aca0bf66aba8d635bd
Subramaniam Swamynathan <ramani1993@gmail.com>
2015-09-21 16:47:49 -0400

    task 8 works

--- rec04/rec04.cpp
+++ rec04/rec04.cpp
@@ -215,6 +215,76 @@ class Sandwich7
     }
 };
 
+class Sandwich8
+{
+  const int numberOfBread = 2;
+  int quantityOfCheese, numberOfTomatoes;
+  double amountOfMustard;
+  double amountOfMayo;
+
+  public:
+    void display()
+  	{  // method that outputs the contents of a sandwich
+  		cout << "Here are the contents of my sandwich" << endl;
+  		cout << "Bread: " << numberOfBread << endl;
+  		cout << "Cheese: " << quantityOfCheese << endl;
+  		cout << "Mayo: " << amountOfMayo << endl;
+  		cout << "Tomoatoes: " << numberOfTomatoes << endl;
+      cout << "Mustard: " << amountOfMustard << endl;
+  	}
+
+    Sandwich8(int numberOfBread = 2,int quantityOfCheese = 1,double amountOfMayo = 0.1 , int numberOfTomatoes = 2, double amountOfMustard = 1.0) : numberOfBread(numberOfBread),quantityOfCheese(quantityOfCheese),numberOfTomatoes(numberOfTomatoes),amountOfMustard(amountOfMustard),amountOfMayo(amountOfMayo)
+    {
+
+    }
+
+    int getBread() const
+    { // return the number of breads in the sandwich
+      return numberOfBread;
+    }
+
+    int getTomatoes() const
+    { // return the number of tomatoes in the sandwich
+      return numberOfTomatoes;
+    }
+
+    int getCheese() const
+    { // return the number of cheese slices in the sandwich
+      return quantityOfCheese;
+    }
+
+    double getMayo() const
+    { // return the amount of mayo in the sandwich
+      return amountOfMayo;
+    }
+
+    double getMustard() const
+    { // return the amount of mustard in the sandwich
+      return amountOfMustard;
+    }
+
+    void setCheese(int newQuantityOfCheese)
+    {// sets the number of cheese slices
+      quantityOfCheese = newQuantityOfCheese;
+    }
+
+    void setTomatoes(int newNumberOfTomatoes)
+    {// sets the number of cheese slices
+      numberOfTomatoes = newNumberOfTomatoes;
+    }
+
+    void setMustard(double newAmountOfMustard)
+    {
+      amountOfMustard = newAmountOfMustard;
+    }
+
+    void setMayo(double newAmountOfMayo)
+    {
+      amountOfMayo = newAmountOfMayo;
+    }
+};
+
+
 int main()
 {
 	Sandwich1 s1;
@@ -239,4 +309,5 @@ int main()
   s7.setMustard(4.0);
   s7.setMayo(4.0);
   s7.display();
+  Sandwich8 s8;
 }
