a515e4378b05c9cfbc1d174a470933e15264540f
Subramaniam Swamynathan <ramani1993@gmail.com>
2015-09-27 02:05:55 -0400

    added functions and some accessors

--- hw02/hw02.cpp
+++ hw02/hw02.cpp
@@ -6,24 +6,59 @@ Program that represents delegation in a warrior and a weapon
 #include<iostream>
 #include<string>
 #include<fstream>
+#include<vector>
 using namespace std;
 
 class Weapon
 {// a type that represents a warrior's weapon. It has two fields a name and a strength
   string name;
   int strength;
+
+  public:
+    string getName() const
+    {// accessor for name
+      return name;
+    }
+
+    int getStrength() const
+    {// accessor for strength
+      return strength;
+    }
+
 };
 
 class Warrior
 {// a type that represents a warrior. It has two fields a name and a weapon... composition
   string name;
   Weapon weapon;
+
+  public:
+    string getName() const
+    {// accessor for name
+      return name;
+    }
+
+    string getWeaponName() const
+    {// delegation of task to the weapon class
+      return weapon.getName();
+    }
+
+    int getWeaponStrength() const
+    {// delegation of task to the weapon class
+      return weapon.getStrength();
+    }
+
 };
+
+void openFile(ifstream&);
+void readFromFile(ifstream&,vector<Warrior>&);
+
 
 int main()
 {
   ifstream ifs;
   openFile(ifs);
+  readFromFile(ifs);
 }
 
 void openFile(ifstream& ifs)
@@ -34,4 +69,39 @@ void openFile(ifstream& ifs)
     cerr << " File cannot be opened ";
     exit(1);
   }
+}
+
+void readFromFile(ifstream& ifs,vector<Warrior>& warriors)
+{//function that reads the input file and processes the commmands
+
+  string command;
+
+  while(ifs >> command)
+  {
+    if (command == "Status")
+    {
+      printWarriors(warriors)
+    }
+
+    else if(command == "Warrior")
+    {
+
+    }
+
+    else if(command == "Battle")
+    {
+
+    }
+  }
+
+}
+
+void printWarriors(const vector<Warrior>& warriors)
+{
+  cout << "There are " << warriors.size() << " warriors";
+  for(size_t i = 0;i<warriors.size();i++)
+  {
+    cout << "Warrior: " << warriors[i].getName() << " , Weapon: " << warriors[i].getWeaponName() <<", " << warriors[i].getWeaponStrength();
+  }
+
 }
