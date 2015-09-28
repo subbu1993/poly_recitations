52fb75626e0a11fe41f59970d1e4753ca901b1fd
Subramaniam Swamynathan <ramani1993@gmail.com>
2015-09-28 16:51:06 -0400

    sure overriding can be done

--- playing_with_inheritance/inheritance.cpp
+++ playing_with_inheritance/inheritance.cpp
@@ -1,27 +1,52 @@
 /*
-  Subbu
-  program to play with inheritance in c++
+Subbu
+program to play with inheritance in c++
 */
+
 #include<iostream>
+#include<string>
 using namespace std;
+
 class Animal
 {
 public:
-  Animal(){
-    cout << "I am an animal" << endl;
+	Animal() {
+		cout << "I am an animal" << endl;
+	}
+
+	Animal(const string& name) : name(name) { cout << "i am called" << endl; }
+	void display() const {
+		cout << name << endl;
+	}
+
+  void eat() const {
+    cout << "animal is eating" << endl;
   }
+private:
+	string name;
 };
 
 class Dog : public Animal
 {
 public:
-  Dog(){
-    cout << "I am a dog" << endl;
+	Dog() {
+		cout << "I am a dog" << endl;
+	}
+
+	Dog(const string& name) : Animal(name) {  }
+
+	void display() const {
+		cout << "i am a dog and my name is ";
+		Animal::display();
+	}
+  void eat() const {
+    cout << "Dog is eating" << endl;
   }
 };
 
 int main()
 {
-  Animal aAnimal;
-  Dog walle;
+	Dog walle("Wall e");
+	walle.display();
+  walle.eat();
 }
