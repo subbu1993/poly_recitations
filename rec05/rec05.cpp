a359d20688ab9478a410c1f6b49be9da62b0c7f9
Subramaniam Swamynathan <ramani1993@gmail.com>
2015-10-06 13:56:24 -0400

    could be inconsistent

--- rec05/rec05.cpp
+++ rec05/rec05.cpp
@@ -1,3 +1,18 @@
 /*
-
+  Subbu
+  Rec05
+  Pointers Task Based
 */
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+  int x;
+  x = 10;
+  cout << "x = " << x << endl;
+  int* p;
+  p = &x;
+  cout << "p = " << p << endl;
+}
